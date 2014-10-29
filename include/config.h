8d1671d186964cb5ed2f9cf9e50b2bb871e7f0b9
Todd Broch <tbroch@chromium.org>
2014-10-28 18:04:01 -0700

    pd: dingdong/hoho: Include product VDO in discovery identity.
    Per table 6-24 of USB PD spec an alternate mode adapter (AMA) should
    include both product & AMA VDOs.
    
    BRANCH=samus
    BUG=chrome-os-partner:31192,chrome-os-partner:31193
    TEST=manual,
    
    Connect hoho/dingdong to fpie/samus and see product VDO proceed the
    AMA VDO in DFP_U console output:
    
        Product VDO -----------------------------v
                                              |------|
        SVDM/5 [1] ff008041 340018d1 00000000 50100001 1100000b
    
        Note, hoho's PID == 0x5010
    
        And dingdong (0x5011)
        SVDM/5 [1] ff008041 340018d1 00000000 50110001 1100000b
    
        Also see bcdDevice field in descriptor match above data.
    
        $ lsusb -v -d 18d1: | egrep -i "idproduct|bcddev"
        idProduct          0x5011
        bcdDevice            0.01
    
    Change-Id: I4d898816a45c68c7ff75a54fd348fc11be408ae0
    Signed-off-by: Todd Broch <tbroch@chromium.org>

--- include/config.h
+++ include/config.h
@@ -1085,6 +1085,9 @@
 /* USB Binary device Object Store support */
 #undef CONFIG_USB_BOS
 
+/* USB Device version of product */
+#undef CONFIG_USB_BCD_DEV
+
 /*****************************************************************************/
 /* USB interfaces config */
 
