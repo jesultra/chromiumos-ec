9ca7b5c1b2c4a24f740989872b44f031c27ed934
Scott <scollyer@chromium.org>
2016-09-01 14:15:14 -0700

    Cr50: Add gpio input for platform reset (plt_rst_l)
    For TPM operation with Intel chipset APs, the signal PLT_RST_L needs
    to trigger a TPM reset. For current Reef boards, this signal is
    connected to DIOA13. The next version will have it on DIOM3.
    
    This CL adds support for platform reset connected on DIOA13 and uses a
    new board property so that it doesn't affect Kevin/Gru.
    
    BRANCH=none
    BUG=chrome-os-partner:55115
    TEST=manual
    Used H1 dev board configured as Reef. Created high to low transisition
    on to verify that platform reset was detected. Tested on Kevin to
    ensure that resets were not occurring.
    
    Change-Id: I58f02b7ffa644a9197f4303ae6e640df181040bd
    Signed-off-by: Scott <scollyer@chromium.org>

--- board/cr50/board.c
+++ board/cr50/board.c
@@ -96,6 +96,7 @@ static void init_pmu(void)
 void pmu_wakeup_interrupt(void)
 {
 	int exiten, wakeup_src;
+	int plt_rst_asserted;
 
 	delay_sleep_by(1 * MSEC);
 
@@ -122,7 +123,13 @@ void pmu_wakeup_interrupt(void)
 		 */
 		delay_sleep_by(3 * MINUTE);
 
-		if (!gpio_get_level(GPIO_SYS_RST_L_IN))
+		/*
+		 * If either SYS_RST_IN_L or PLT_RST_IN are low, then assume
+		 * reset was triggered while Cr50 was sleeping.
+		 */
+		plt_rst_asserted = board_properties & BOARD_USE_PLT_RESET ?
+			!gpio_get_level(GPIO_PLT_RST_L) : 0;
+		if (!gpio_get_level(GPIO_SYS_RST_L_IN) || plt_rst_asserted)
 			sys_rst_asserted(GPIO_SYS_RST_L_IN);
 	}
 
@@ -198,6 +205,29 @@ static void configure_board_specific_gpios(void)
 	/* Add a pullup to sys_rst_l */
 	if (system_get_board_properties() & BOARD_NEEDS_SYS_RST_PULL_UP)
 		GWRITE_FIELD(PINMUX, DIOM0_CTL, PU, 1);
+
+	/*
+	 * TODO(crosbug.com/p/56540): Need to connect platform reset to DI0A13
+	 * for current Reef boards. This function is a no-op for Kevin/Gru. When
+	 * platform reset is moved to DIOM3 in HW, then need change to
+	 * GC_PINMUX_DIOM3_SEL and DIOM3_CTL respectively. In addition,
+	 * uncomment the 3 GRWITE() lines for enabling wake on falling edge.
+	 */
+	/* Connect PLT_RST_L signal to the pinmux */
+	if (system_get_board_properties() & BOARD_USE_PLT_RESET) {
+		/* Signal using GPIO1 pin 10 for DIOA13 */
+		GWRITE(PINMUX, GPIO1_GPIO10_SEL, GC_PINMUX_DIOA13_SEL);
+		/* Enbale the input */
+		GWRITE_FIELD(PINMUX, DIOA13_CTL, IE, 1);
+
+		/* Set power down for the equivalent of DIO_WAKE_FALLING */
+		/* Enable powerdown exit on DIOM3 */
+		/* GWRITE_FIELD(PINMUX, EXITEN0, DIOM3, 1); */
+		/* Set to be edge sensitive */
+		/* GWRITE_FIELD(PINMUX, EXITEDGE0, DIOM3, 1); */
+		/* Select failling edge polarity */
+		/* GWRITE_FIELD(PINMUX, EXITINV0, DIOM3, 1); */
+	}
 }
 
 /* Initialize board. */
@@ -584,6 +614,11 @@ void system_init_board_properties(void)
 			 * benchmark for marking the updated image as good.
 			 */
 			properties |= BOARD_MARK_UPDATE_ON_USB_REQ;
+			/*
+			 * Platform reset is present and will need to be
+			 * configured as a an falling edge interrupt.
+			 */
+			properties |= BOARD_USE_PLT_RESET;
 		}
 
 		/*
