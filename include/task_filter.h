jackadam1981/ChromeEc

ec: Add analysis tool for firmware stack usage.
Add a static analysis tool for firmware stack usage.
Add an new Makefile rule to analyze the stack usages of firmwares.

Details about the tool can be found in extra/stack_analyzer/README.md.

BUG=chromium:648840
BRANCH=none
TEST=extra/stack_analyzer/stack_analyzer_unittest.py
     make BOARD=${BOARD} SECTION=${SECTION} analyzestack

Change-Id: Ifb1b5f5ad6be8f8b125b14d6ee03e25cb385895b
Signed-off-by: Che-yu Wu <cheyuw@google.com>

include/task_filter.h
@@ -46,4 +46,11 @@
 CONFIG_TASK_LIST CONFIG_TEST_TASK_LIST CONFIG_CTS_TASK_LIST
 #endif
 
+/* If included directly from Makefile, dump details of task list. */
+#ifdef _MAKEFILE_DETAIL
+#define TASK(n, r, d, s) (#n, r, s)
+CONFIG_TASK_LIST CONFIG_TEST_TASK_LIST CONFIG_CTS_TASK_LIST
+#endif
+
+
 #endif /*  __CROS_EC_TASK_FILTER_H */

jesultra/chromiumos-ec

     make BOARD=elm && extra/stack_analyzer/stack_analyzer.py \
         --objdump=arm-none-eabi-objdump \
         --addr2line=arm-none-eabi-addr2line \
         ./build/elm/RW/ec.RW.elf ./build/elm/RW/ec.RW.taskinfo

include/task_filter.h
@@ -46,4 +46,11 @@
 CONFIG_TASK_LIST CONFIG_TEST_TASK_LIST CONFIG_CTS_TASK_LIST
 #endif
 
+/* If included directly from Makefile, dump details of task list. */
+#ifdef _MAKEFILE_DUMP_INFO
+#define TASK(n, r, d, s) (#n, r, s)
+CONFIG_TASK_LIST CONFIG_TEST_TASK_LIST CONFIG_CTS_TASK_LIST
+#endif
+
+
 #endif /*  __CROS_EC_TASK_FILTER_H */