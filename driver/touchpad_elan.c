jackadam1981/ChromeEc

whiskers: add touchpad_passthru
BUG=b:70482333
TEST=None
BRANCH=hammer
Signed-off-by: Wei-Han Chen <stimim@google.com>

Change-Id: Ie7a4c02c2443e3f019c74de3be3891e4811381e0

driver/touchpad_elan.c
@@ -15,6 +15,7 @@
 #include "update_fw.h"
 #include "util.h"
 #include "usb_hid_touchpad.h"
+#include "touchpad_passthru.h"
 
 /* Console output macros */
 #define CPUTS(outstr) cputs(CC_TOUCHPAD, outstr)
@@ -228,6 +229,9 @@ static int elan_tp_read_report(void)
 	}
 
 	set_touchpad_report(&report);
+#ifdef USB_TOUCHPAD_PASSTHRU
+	touchpad_passthru_generate_event();
+#endif
 
 	return 0;
 }

jesultra/chromiumos-ec

driver/touchpad_elan.c
@@ -15,6 +15,7 @@
 #include "update_fw.h"
 #include "util.h"
 #include "usb_hid_touchpad.h"
+#include "touchpad_passthru.h"
 
 /* Console output macros */
 #define CPUTS(outstr) cputs(CC_TOUCHPAD, outstr)
@@ -228,6 +229,9 @@ static int elan_tp_read_report(void)
 	}
 
 	set_touchpad_report(&report);
+#ifdef CONFIG_USB_TOUCHPAD_PASSTHRU
+	touchpad_passthru_generate_event();
+#endif
 
 	return 0;
 }