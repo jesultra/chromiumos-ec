1971b65f422fe2c88b7f600baf824f22185aebf3
Diana Z <dzigterman@chromium.org>
2018-09-27 14:55:27 -0600

    PD 3.0: Send Reject messages for refusing swaps
    When refusing a PR_Swap, DR_Swap, or VCONN_Swap the Reject control message
    needs to be used rather than Not_supported.
    
    BRANCH=None
    BUG=b:64411727
    TEST=tested with a PD 3.0 hub which always requests a DR_swap upon
    connection.  Previously, it would leave its cc line at NG upon receiving
    the unexpected Not_supported response and it now leaves the line at OK.
    
    Change-Id: Ifafbadece5c45e51f4100be5e3590c07fcb27346
    Signed-off-by: Diana Z <dzigterman@chromium.org>

--- common/usb_pd_protocol.c
+++ common/usb_pd_protocol.c
@@ -121,16 +121,13 @@ static uint8_t pd_try_src_enable;
 
 #ifdef CONFIG_USB_PD_REV30
 /*
- * The spec. revision is used to index into this array.
- *  Rev 0 (PD 1.0) - return PD_CTRL_REJECT
- *  Rev 1 (PD 2.0) - return PD_CTRL_REJECT
- *  Rev 2 (PD 3.0) - return PD_CTRL_NOT_SUPPORTED
+ * Note: this should only be used in locations where responding on a lower
+ * revision with a Reject is valid (ex. a source refusing a PR_Swap).  For
+ * other uses of Not_Supported, use PD_CTRL_NOT_SUPPORTED directly.
  */
-static const uint8_t refuse[] = {
-	PD_CTRL_REJECT, PD_CTRL_REJECT, PD_CTRL_NOT_SUPPORTED};
-#define REFUSE(r) refuse[r]
+#define NOT_SUPPORTED(r) (r < 2 ? PD_CTRL_REJECT : PD_CTRL_NOT_SUPPORTED)
 #else
-#define REFUSE(r) PD_CTRL_REJECT
+#define NOT_SUPPORTED(r) PD_CTRL_REJECT
 #endif
 
 #ifdef CONFIG_USB_PD_REV30
@@ -1545,7 +1542,7 @@ static void handle_ctrl_request(int port, uint16_t head,
 #ifdef CONFIG_USB_PD_DUAL_ROLE
 		send_sink_cap(port);
 #else
-		send_control(port, REFUSE(pd[port].rev));
+		send_control(port, NOT_SUPPORTED(pd[port].rev));
 #endif
 		break;
 #ifdef CONFIG_USB_PD_DUAL_ROLE
@@ -1728,10 +1725,10 @@ static void handle_ctrl_request(int port, uint16_t head,
 					PD_STATE_SNK_SWAP_SNK_DISABLE,
 					PD_STATE_SRC_SWAP_SNK_DISABLE));
 		} else {
-			send_control(port, REFUSE(pd[port].rev));
+			send_control(port, PD_CTRL_REJECT);
 		}
 #else
-		send_control(port, REFUSE(pd[port].rev));
+		send_control(port, NOT_SUPPORTED(pd[port].rev));
 #endif
 		break;
 	case PD_CTRL_DR_SWAP:
@@ -1745,7 +1742,7 @@ static void handle_ctrl_request(int port, uint16_t head,
 			if (send_control(port, PD_CTRL_ACCEPT) >= 0)
 				pd_dr_swap(port);
 		} else {
-			send_control(port, REFUSE(pd[port].rev));
+			send_control(port, PD_CTRL_REJECT);
 
 		}
 		break;
@@ -1758,11 +1755,11 @@ static void handle_ctrl_request(int port, uint16_t head,
 					set_state(port,
 						  PD_STATE_VCONN_SWAP_INIT);
 			} else {
-				send_control(port, REFUSE(pd[port].rev));
+				send_control(port, PD_CTRL_REJECT);
 			}
 		}
 #else
-		send_control(port, REFUSE(pd[port].rev));
+		send_control(port, NOT_SUPPORTED(pd[port].rev));
 #endif
 		break;
 	default:
