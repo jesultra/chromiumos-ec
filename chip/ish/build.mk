jackadam1981/ChromeEc

ish: add memory dma driver for ISH5
Arcada/ISH5 power management needs the support for dma between
UMA and SRAM to do SRAM swap in and swap out. Add the dma driver
and API which utilizes dma engine to perform dma transfer between
UMA and SRAM.

BUG=b:127723182
BRANCH=none
TEST=tested on arcada

Signed-off-by: Leifu Zhao <leifu.zhao@intel.com>
Change-Id: I22172b176eed92d7f487641f9b5c79dfd04f602a

chip/ish/build.mk
@@ -25,6 +25,7 @@ chip-$(CONFIG_HOSTCMD_HECI)+=host_command_heci.o
 chip-$(CONFIG_HOSTCMD_HECI)+=heci.o system_state_subsys.o ipc_heci.o
 chip-$(CONFIG_HID_HECI)+=hid_subsys.o
 chip-$(CONFIG_HID_HECI)+=heci.o system_state_subsys.o ipc_heci.o
+chip-$(CHIP_FAMILY_ISH5)+=dma.o
 
 # location of the scripts and keys used to pack the SPI flash image
 SCRIPTDIR:=./chip/${CHIP}/util

jesultra/chromiumos-ec

chip/ish/build.mk
@@ -25,6 +25,7 @@ chip-$(CONFIG_HOSTCMD_HECI)+=host_command_heci.o
 chip-$(CONFIG_HOSTCMD_HECI)+=heci.o system_state_subsys.o ipc_heci.o
 chip-$(CONFIG_HID_HECI)+=hid_subsys.o
 chip-$(CONFIG_HID_HECI)+=heci.o system_state_subsys.o ipc_heci.o
+chip-$(CONFIG_DMA_PAGING)+=dma.o
 
 # location of the scripts and keys used to pack the SPI flash image
 SCRIPTDIR:=./chip/${CHIP}/util