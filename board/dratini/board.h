9710bb551e240bdefde3145bda6813a4e5ff5f9a
Devin Lu <devin.lu@quantatw.com>
2019-09-10 10:58:54 +0800

    dratini: add batteries configuration
    Add batteries configuration as following:
    
    1. LGC MPPHPPBC031C
    2. Simplo 996QA182H
    
    BUG=b:140635151
    BRANCH=none
    TEST=make buildall -j
    
    Change-Id: I69eb4b256127a3e7a53fed914406626b34460910
    Signed-off-by: Devin Lu <Devin.Lu@quantatw.com>

--- board/dratini/board.h
+++ board/dratini/board.h
@@ -166,8 +166,8 @@ enum temp_sensor_id {
 
 /* List of possible batteries */
 enum battery_type {
-	BATTERY_SMP_LIS,
-	BATTERY_SMP_SDI,
+	BATTERY_SIMPLO_COS,
+	BATTERY_LGC,
 	BATTERY_TYPE_COUNT,
 };
 
