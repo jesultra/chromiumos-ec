29084170f6f2ab651118dc1556d567a9509892b4
Sue Chen <sue.chen@quanta.corp-partner.google.com>
2021-09-11 15:47:18 +0800

    config: set CONFIG_BATT_HOST_SHUTDOWN_PERCENTAGE to 3
    For BATTERY_LEVEL_SHUTDOWN > CONFIG_BATT_HOST_SHUTDOWN_PERCENTAGE,
    EC triggers critical shutdown earlier than host triggers low-battery
    shutdown. It would make battey test failed. Therefore, setting
    CONFIG_BATT_HOST_SHUTDOWN_PERCENTAGE >= BATTERY_LEVEL_SHUTDOWN to let
    host trigger low-battery shutdown.
    
    BUG=none
    BRANCH=icarus
    TEST=make sure system log shows "Shutting down, reason: low-battery"
    
    Signed-off-by: Sue Chen <sue.chen@quanta.corp-partner.google.com>
    Change-Id: If9580b3d162a92d671a28862b7ee8224ec3e0e48

--- include/config.h
+++ include/config.h
@@ -657,7 +657,7 @@
  * Thus, we set them as follows by default:
  *
  *   CONFIG_CHARGER_MIN_BAT_PCT_FOR_POWER_ON = 2 (don't boot if soc < 2%)
- *   CONFIG_BATT_HOST_SHUTDOWN_PERCENTAGE = 2    (shutdown if soc <= 2%)
+ *   CONFIG_BATT_HOST_SHUTDOWN_PERCENTAGE = 3    (shutdown if soc <= 3%)
  *   BATTERY_LEVEL_SHUTDOWN = 3                  (shutdown if soc < 3%)
  *
  * This produces the following behavior:
@@ -668,7 +668,7 @@
  *   If that happens while a user is away, they can press the power button to
  *   learn what happened.
  */
-#define CONFIG_BATT_HOST_SHUTDOWN_PERCENTAGE	2  /* shutdown if soc <= 2% */
+#define CONFIG_BATT_HOST_SHUTDOWN_PERCENTAGE	3  /* shutdown if soc <= 3% */
 
 /*
  * Powerd's full_factor. The value comes from:
