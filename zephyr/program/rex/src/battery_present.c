5decf4dadc75a39a12981b6034fd251753a5f041
Caveh Jalali <caveh@chromium.org>
2023-07-13 23:29:06 -0700

    rex: Enable BATTERY_HW_PRESENT_CUSTOM
    This enables the battery_hw_present() check which uses a GPIO to
    mechanically detect the presence of the system battery. This enables
    some additional system power-loss avoidance checks where appropriate.
    
    This is particularly relevant because in modern usage
    battery_is_present() is more of a logical "battery usable" check rather
    than a physical battery presence check.
    
    BUG=b:289566759
    TEST='./twister -T zephyr/test/rex' passes
    
    Change-Id: I1f5889b3012adba157e1da56545d7300c7c1dd02

--- zephyr/program/rex/src/battery_present.c
+++ zephyr/program/rex/src/battery_present.c
@@ -9,7 +9,7 @@
 /*
  * Physical detection of battery.
  */
-static enum battery_present rex_battery_hw_present(void)
+enum battery_present battery_hw_present(void)
 {
 	const struct gpio_dt_spec *batt_pres;
 
@@ -27,7 +27,7 @@ enum battery_present battery_is_present(void)
 		return BP_NO;
 
 	/* Get the physical hardware status */
-	batt_pres = rex_battery_hw_present();
+	batt_pres = battery_hw_present();
 
 	return batt_pres;
 }
