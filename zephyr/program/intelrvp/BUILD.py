bd350838377dadc05aa2f4ae81a9dd4e8df99b17
Jay Patel <jay2.patel@intel.com>
2023-08-07 18:49:31 -0700

    program/mtlrvp/mtlrvpp_pd: Create project mtlrvpp_pd
    Create initial project mtlrvpp_pd for introducing PD chips for
    chrome-OS
    
    Change-Id: I859e222e2425f24dff6032c9363ca708c521e6fe
    Signed-off-by: Jay Patel <jay2.patel@intel.com>

--- zephyr/program/intelrvp/BUILD.py
+++ zephyr/program/intelrvp/BUILD.py
@@ -118,3 +118,23 @@ register_intelrvp_project(
         here / "mtlrvp/mtlrvpp_npcx/prj.conf",
     ],
 )
+
+register_intelrvp_project(
+    project_name="mtlrvpp_pd",
+    chip="npcx9m3f",
+    extra_dts_overlays=[
+        here / "mtlrvp/mtlrvpp_npcx/fan.dts",
+        here / "mtlrvp/mtlrvpp_npcx/gpio.dts",
+        here / "mtlrvp/mtlrvpp_npcx/keyboard.dts",
+        here / "mtlrvp/mtlrvpp_npcx/interrupts.dts",
+        here / "mtlrvp/ioex.dts",
+        here / "mtlrvp/mtlrvpp_pd/mtlrvpp_pd.dts",
+        here / "mtlrvp/mtlrvpp_npcx/mtlrvp_npcx_power_signals.dts",
+        here / "adlrvp/adlrvp_npcx/temp_sensor.dts",
+        here / "mtlrvp/mtlrvpp_pd/usbc.dts",
+    ],
+    extra_kconfig_files=[
+        here / "zephyr_ap_pwrseq.conf",
+        here / "mtlrvp/mtlrvpp_npcx/prj.conf",
+    ],
+)
