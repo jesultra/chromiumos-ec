9ef562f28b4447dc2ce57f3f363a19fb55ca57e6
Ridden_Liu <ridden_liu@compal.corp-partner.google.com>
2023-12-05 09:43:15 +0800

    Uldren: Create zephyr test for uldren
    Create zephyr test for uldren
    
    charger.c test pass
    
    BUG=b:291835976
    TEST=./twister -T zephyr/test/nissa  --coverage
    
    Change-Id: Ifc90c02e139467d5bad6ddfd03fe4e88feed898f

---
+++ zephyr/test/nissa/include/uldren.h
@@ -0,0 +1,14 @@
+/* Copyright 2024 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef ZEPHYR_TEST_NISSA_INCLUDE_ULDREN_H_
+#define ZEPHYR_TEST_NISSA_INCLUDE_ULDREN_H_
+
+#include "ec_commands.h"
+
+extern int battery_fuel_gauge_type_override;
+extern int charge_port;
+
+#endif /* ZEPHYR_TEST_NISSA_INCLUDE_ULDREN_H_ */
